cffc61c5579abb1ff85c9938d8f91dc451d0aea5
CrumrineCoder <crumrinecoding@gmail.com>
2019-03-12 04:07:55 -0400

    Added images; clothing boxes are aligned to the top

--- client/src/App/categories/containers/CategoriesContainer.js
+++ client/src/App/categories/containers/CategoriesContainer.js
@@ -56,43 +56,43 @@ class CategoriesContainer extends Component {
 
         var dresses = [
             {
-                name: "Unique Vintage Black Iridescent Beaded Zelia Fringe Flapper Dress",
+                name: "Deco Purple & Black Sequin Veronique Fringe Flapper Dress",
                 brandName: "Gibson Girl",
                 images: [
-                    "Dresses/74683/74683_1_2048x2048.jpg",
-                    "Dresses/74683/74683_2_2048x2048.jpg",
-                    "Dresses/74683/74683_3_1024x1024.jpg",
-                    "Dresses/74683/74683_4_1024x1024.jpg",
-                    "Dresses/74683/74683_5_2048x2048.jpg"
+                    "Dresses/61626/61626-1_2048x2048.jpg",
+                    "Dresses/61626/61626-2_2048x2048.jpg",
+                    "Dresses/61626/61626-3_2048x2048.jpg",
+                    "Dresses/61626/61626-4_2048x2048.jpg",
+                    "Dresses/61626/61626-5_2048x2048.jpg"
+                ],
+                price: 14.00
+            },
+            {
+                name: "Navy Hemingway Flapper Dress",
+                brandName: "Ryan Boutique",
+                images: [
+                    "Dresses/1606/Unique_Vintage_Navy_Hemingway_Flapper_Dress_6_2048x2048.jpg",
+                    "Dresses/1606/Unique_Vintage_Navy_Hemingway_Flapper_Dress_2_2048x2048",
+                    "Dresses/1606/Unique_Vintage_Navy_Hemingway_Flapper_Dress_4_2048x2048.jpg",
+                    "Dresses/1606/Unique_Vintage_Navy_Hemingway_Flapper_Dress_2048x2048.jpg",
+                    "Dresses/1606/Unique_Vintage_Navy_Hemingway_Flapper_Dress_3_1024x1024.jpg"
+                ],
+                price: 74.00
+            },
+            {
+                name: "Black & Green Beaded Antoinette Peacock Flapper",
+                brandName: "Apollo",
+                images: [
+                    "Dresses/53556/Unique_Vintage_1920s_Black_Green_Beaded_Antoinette_Peacock_Flapper_2_f3b364a1-4a7a-4a7a-a1d8-9657de078c3b_2048x2048.jpg",
+                    "Dresses/53556/Unique_Vintage_1920s_Black_Green_Beaded_Antoinette_Peacock_Flapper_5_1d72e5c4-3365-4fcc-83da-fa85c7f33bb8_2048x2048.jpg",
+                    "Dresses/53556/Unique_Vintage_1920s_Black_Green_Beaded_Antoinette_Peacock_Flapper_6_3a343f09-e1bd-4224-847e-7a4c497698ef_2048x2048.jpg",
+                    "Dresses/53556/Unique_Vintage_1920s_Black_Green_Beaded_Antoinette_Peacock_Flapper_c6a4d060-a7a5-4825-9e4b-4bbe641b95e4_2048x2048.jpg",
+                    "Dresses/53556/53556_L_2048x2048.jpg"
                 ],
                 price: 98.00
             },
             {
-                name: "Unique Vintage Black Iridescent Beaded Zelia Fringe Flapper Dress",
-                brandName: "Ryan Boutique",
-                images: [
-                    "Dresses/74683/74683_1_2048x2048.jpg",
-                    "Dresses/74683/74683_2_2048x2048.jpg",
-                    "Dresses/74683/74683_3_1024x1024.jpg",
-                    "Dresses/74683/74683_4_1024x1024.jpg",
-                    "Dresses/74683/74683_5_2048x2048.jpg"
-                ],
-                price: 103.00
-            },
-            {
-                name: "Unique Vintage Black Iridescent Beaded Zelia Fringe Flapper Dress",
-                brandName: "Apollo",
-                images: [
-                    "Dresses/74683/74683_1_2048x2048.jpg",
-                    "Dresses/74683/74683_2_2048x2048.jpg",
-                    "Dresses/74683/74683_3_1024x1024.jpg",
-                    "Dresses/74683/74683_4_1024x1024.jpg",
-                    "Dresses/74683/74683_5_2048x2048.jpg"
-                ],
-                price: 23.00
-            },
-            {
-                name: "Unique Vintage Black Iridescent Beaded Zelia Fringe Flapper Dress",
+                name: "Black Iridescent Beaded Zelia Fringe Flapper Dress",
                 brandName: "ÁVELINE'S",
                 images: [
                     "Dresses/74683/74683_1_2048x2048.jpg",
@@ -101,7 +101,19 @@ class CategoriesContainer extends Component {
                     "Dresses/74683/74683_4_1024x1024.jpg",
                     "Dresses/74683/74683_5_2048x2048.jpg"
                 ],
-                price: 294.00
+                price: 300.00
+            },
+            {
+                name: "Style Champagne Beaded Fringe Aurore Flapper Dress",
+                brandName: "ÁVELINE'S",
+                images: [
+                    "Dresses/43915/s191854_1_aaca9fce-1f27-494b-9239-f6203eb48441_2048x2048.jpg",
+                    "Dresses/43915/s191854_2_f761fe2a-3bcb-4edd-a1bd-b9ba96b781e4_1024x1024.jpg",
+                    "Dresses/43915/s191854_3_6c8f29ca-efa0-48f4-95a4-293765309c62_2048x2048.jpg",
+                    "Dresses/43915/s191854_4_658e7f58-948d-418a-a9bd-3e44f4a1a245_1024x1024.jpg",
+                    "Dresses/43915/s191854_5_a86de98f-f546-4480-bc11-a20d3ecd1a31_2048x2048.jpg"
+                ],
+                price: 205.00
             }
         ]
 
@@ -117,15 +129,12 @@ class CategoriesContainer extends Component {
                 </ul>
             )
         } else {
-            console.log("jive");
             pageContent = (
                 <ul className="polls">
                     {dresses.map((dress, i) => <ClothingBox key={i} dress={dress} />)}
                 </ul>
             )
         }
-
-        console.log(pageContent);
 
         return (
             <div className="categoriesContainer">
