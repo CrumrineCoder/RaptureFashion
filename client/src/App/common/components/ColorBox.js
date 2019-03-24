e77517e88a0880b7b5b36a550191a52c9abf714d
CrumrineCoder <crumrinecoding@gmail.com>
2019-03-24 13:19:41 -0400

    Starting to work on color box

---
+++ client/src/App/common/components/ColorBox.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class ColorBox extends Component {
+
+    constructor(props) {
+        super(props);
+        // initial state stores the questions, an array of answers the user will input, options the user will make true or false, linked represents if the user will be linked, and submitted checks if the poll has been submitted yet. 
+        this.state = {
+        }
+    }
+
+    render(){
+        // TO DO: ADD LINK
+        return(
+            <div className="ColorBox">
+               
+            </div>
+        )
+    }
+}
+
+export default ColorBox;
