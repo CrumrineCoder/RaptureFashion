a4d43f18fc28812fcc6a3340c0cdfcdfcaae4113
CrumrineCoder <crumrinecoding@gmail.com>
2019-03-29 11:41:28 -0400

    State interaction

--- client/src/App/checkout/containers/CheckoutContainer.js
+++ client/src/App/checkout/containers/CheckoutContainer.js
@@ -20,18 +20,25 @@ class CheckoutContainer extends Component {
         super(props);
         // Used for when searching and tagging functionality whenever that comes
         this.state = {
-            email: ""
+            email: "",
+            fName: "",
+            lName: "",
+            address: "",
+            apartment: "",
+            city: "",
+            country: "",
+            phone: ""
         }
         this.handleInputChange = this.handleInputChange.bind(this);
     }
 
     handleInputChange(event) {
         const target = event.target;
-        const value =  target.value;
+        const value = target.value;
         const name = target.name;
-    
+
         this.setState({
-          [name]: value
+            [name]: value
         });
     }
 
@@ -42,23 +49,22 @@ class CheckoutContainer extends Component {
             <div className="">
                 <p>Already have an account? Log In</p>
                 <h3>Contact Information</h3>
-                {this.state.email}
+                {this.state.country}
                 <input type="text" placeholder="Email" name="email" onChange={this.handleInputChange} value={this.state.email}></input>
                 <h3>Shipping Address</h3>
-                <input type="text" placeholder="First name"></input>
-                <input type="text" placeholder="Last name"></input>
-                <input type="text" placeholder="Address"></input>
-                <input type="text" placeholder="Apartment, suite, etc. (optional)"></input>
-                <input type="text" placeholder="City"></input>
-                <label for="country">Country:</label>
-                <input list="countries" id="country" name="country" />
-                <datalist id="countries">
-                    <option value="Canada" />
-                    <option value="Rapture" />
-                    <option value="United Kingdom" />
-                    <option value="United States" />
-                </datalist>
-                <input type="text" placeholder="Phone"></input>
+                <input type="text" placeholder="First name" name="fName" onChange={this.handleInputChange} value={this.state.fName}></input>
+                <input type="text" placeholder="Last name" name="lName" onChange={this.handleInputChange} value={this.state.lName}></input>
+                <input type="text" placeholder="Address" name="address" onChange={this.handleInputChange} value={this.state.address}></input>
+                <input type="text" placeholder="Apartment, suite, etc. (optional)" name="apartment" onChange={this.handleInputChange} value={this.state.apartment}></input>
+                <input type="text" placeholder="City" name="city" onChange={this.handleInputChange} value={this.state.city}></input>
+                <select name="country" onChange={this.handleInputChange} value={this.state.country}>
+                    <option value="NULL">Select a Country</option>
+                    <option value="Canada">Canada</option>
+                    <option value="Rapture">Rapture</option>
+                    <option value="United Kingdom">United Kingdom</option>
+                    <option value="United States">United States</option>
+                </select>
+                <input type="text" placeholder="Phone" name="phone" onChange={this.handleInputChange} value={this.state.phone}></input>
             </div>
         );
 
