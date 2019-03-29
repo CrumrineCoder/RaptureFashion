52e0d65953399f86af28b73f190f7280a22b4af7
CrumrineCoder <crumrinecoding@gmail.com>
2019-03-29 11:52:26 -0400

    Created new component for address

--- client/src/App/checkout/containers/CheckoutContainer.js
+++ client/src/App/checkout/containers/CheckoutContainer.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import ClothingBox from '../../home/components/ClothingBox';
 import { cartActions } from '../../_actions/cart.actions.js';
 import { Link } from 'react-router-dom';
+import Address from "../components/address.js";
 /*
 import ClothingBox from '../components/ClothingBox';
 import CategoriesBox from '../components/CategoriesBox';
@@ -20,51 +21,18 @@ class CheckoutContainer extends Component {
         super(props);
         // Used for when searching and tagging functionality whenever that comes
         this.state = {
-            email: "",
-            fName: "",
-            lName: "",
-            address: "",
-            apartment: "",
-            city: "",
-            country: "",
-            phone: ""
+            address: {}
         }
-        this.handleInputChange = this.handleInputChange.bind(this);
     }
 
-    handleInputChange(event) {
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
 
-        this.setState({
-            [name]: value
-        });
-    }
 
 
     render() {
 
         return (
             <div className="">
-                <p>Already have an account? Log In</p>
-                <h3>Contact Information</h3>
-                {this.state.country}
-                <input type="text" placeholder="Email" name="email" onChange={this.handleInputChange} value={this.state.email}></input>
-                <h3>Shipping Address</h3>
-                <input type="text" placeholder="First name" name="fName" onChange={this.handleInputChange} value={this.state.fName}></input>
-                <input type="text" placeholder="Last name" name="lName" onChange={this.handleInputChange} value={this.state.lName}></input>
-                <input type="text" placeholder="Address" name="address" onChange={this.handleInputChange} value={this.state.address}></input>
-                <input type="text" placeholder="Apartment, suite, etc. (optional)" name="apartment" onChange={this.handleInputChange} value={this.state.apartment}></input>
-                <input type="text" placeholder="City" name="city" onChange={this.handleInputChange} value={this.state.city}></input>
-                <select name="country" onChange={this.handleInputChange} value={this.state.country}>
-                    <option value="NULL">Select a Country</option>
-                    <option value="Canada">Canada</option>
-                    <option value="Rapture">Rapture</option>
-                    <option value="United Kingdom">United Kingdom</option>
-                    <option value="United States">United States</option>
-                </select>
-                <input type="text" placeholder="Phone" name="phone" onChange={this.handleInputChange} value={this.state.phone}></input>
+                <Address></Address>
             </div>
         );
 
