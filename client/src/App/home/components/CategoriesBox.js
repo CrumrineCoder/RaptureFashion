28f691a365bee7aef144582cf00ad6b4a22fbabb
CrumrineCoder <crumrinecoding@gmail.com>
2019-04-21 10:39:38 -0400

    Working on hover effect

--- client/src/App/home/components/CategoriesBox.js
+++ client/src/App/home/components/CategoriesBox.js
@@ -7,22 +7,53 @@ class CategoriesBox extends Component {
         super(props);
         // initial state stores the questions, an array of answers the user will input, options the user will make true or false, linked represents if the user will be linked, and submitted checks if the poll has been submitted yet. 
         this.state = {
-            box: {}
+            box: {},
+            hover: false
         }
+        this.hoverStart = this.hoverStart.bind(this);
+        this.hoverEnd = this.hoverEnd.bind(this);
     }
 
     componentWillMount() {
         this.setState({ box: this.props.box });
     }
 
+    hoverStart() {
+        this.setState({
+            hover: true
+        })
+    }
+
+    hoverEnd() {
+        this.setState({
+            hover: false
+        })
+    }
+
     render() {
         // TO DO: ADD LINK
+        console.log("PROPSAROONI", this.props);
+        let brandText = ""
+        if (this.props.box.name == "Gibson Girls") {
+
+        } else if (this.props.box.name == "Ryan Boutique") {
+
+        } else if (this.props.box.name == "Apollo") {
+
+        } else if (this.props.box.name == "ÁVELINE'S") {
+            brandText = "Áveline is one of the emerging young designers in Rapture  who came to spotlight before she graduated in 2009 at Contemporary Arts Institute Rapture. That year she became the top young fashion designer at the Fashion Awards Rapture. Áveline plays an important role in the Rapture contemporary fashion scene, she won the Best Fashion Designer Award of Glamour Women of the Year in 2014. She is on the right track to share her powerful and vibrant creations with a wider audience. \n Her independent Rapture based label's vision contains innovative, modern design for a cosmoplitan woman who is self-assured and proud of her individual image; the woman who values the expression of originality in her day-to-day life. The love for contrasts is reflected throughout every of Áveline's work, she experiments a lot with silhouettes, shapes, textures and colours. Inspiration drawn from contemporary art and youth culture is combined with an emphasis on tailoring and the use of custom developed fabrics. This playful and fresh attitude presents the effortless 'power-woman' blend of the 20th and 21st century."
+        }
+        brandText = brandText.split('\n').map((item, i) => {
+            return <p key={i}>{item}</p>;
+        });
         return (
-            <Link className="categoriesBox" to={"/categories/"+this.props.category} >
+            <Link onMouseEnter={this.hoverStart} onMouseLeave={this.hoverEnd} className="categoriesBox" to={"/categories/" + this.props.category} >
                 <div>
                     <img className="categoriesBoxImage rounded" src={require("../../../assets/" + this.state.box.image)} />
                     <p className="categoriesBoxTag">{this.state.box.name}</p>
+                    <p className="categoriesBoxTag">{String(this.state.hover)}</p>
                 </div>
+
             </Link>
         )
     }
