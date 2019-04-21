2750e2e453267929c2479adf5a462ca769468f51
CrumrineCoder <crumrinecoding@gmail.com>
2019-04-21 09:59:41 -0400

    Experimenting with adding text to brand pages

--- README.md
+++ README.md
@@ -19,6 +19,15 @@ We are passionate about allowing the brighest of minds in Rapture to provide tho
 
 We strongly believe that the Rapture Fashion Creative Lab is the future of fashion incubation. It's a progressive platform for exploring fresh new ideas with emerging independent designers and visionary conceptual thinkers. WE harness our remarkable manufacturing abilities and retail presence to expose these creative indviduals to a rapidly growing international audience. The results are amazing - call it Collaboration 2.0. 
 
+Áveline is one of the emerging young designers in Rapture  who came to spotlight before she graduated in 2009 at Contemporary Arts Institute Rapture. That year she became the top young fashion designer at the Fashion Awards Rapture. Áveline plays an important role in the Rapture contemporary fashion scene, she won the Best Fashion Designer Award of Glamour Women of the Year in 2014. She is on the right track to share her powerful and vibrant creations with a wider audience.
+ 
+Her independent Rapture based label's vision contains innovative, modern design for a cosmoplitan woman who is self-assured and proud of her individual image; the woman who values the expression of originality in her day-to-day life. The love for contrasts is reflected throughout every of Áveline's work, she experiments a lot with silhouettes, shapes, textures and colours. Inspiration drawn from contemporary art and youth culture is combined with an emphasis on tailoring and the use of custom developed fabrics. This playful and fresh attitude presents the effortless 'power-woman' blend of the 20th and 21st century.
+
+
+
+
+Maybe the Brand stuff on the home page should expand to explain the brand, with some examples. 
+
 
 Check if in stock
 
--- client/src/App/categories/containers/CategoriesContainer.js
+++ client/src/App/categories/containers/CategoriesContainer.js
@@ -744,7 +744,7 @@ class CategoriesContainer extends Component {
             }
         ]
 
-        let vendorHeader; 
+        let vendorHeader;
         const state = store.getState().home.cart; // state from redux store
         let clothing = state.products;
         if (this.props.clothing != "all") {
@@ -769,6 +769,7 @@ class CategoriesContainer extends Component {
                     }
                 } else if (this.props.vendor) {
                     let vendorImage;
+                    let vendorText;
                     if (this.props.vendor == "Gibson Girls") {
                         clothing = state.products.filter(function (a) {
                             return a.vendor == "Gibson Girls"
@@ -789,13 +790,25 @@ class CategoriesContainer extends Component {
                             return a.vendor == "ÁVELINE'S"
                         });
                         vendorImage = "Brand/aveline.png";
+                        vendorText = "Áveline is one of the emerging young designers in Rapture  who came to spotlight before she graduated in 2009 at Contemporary Arts Institute Rapture. That year she became the top young fashion designer at the Fashion Awards Rapture. Áveline plays an important role in the Rapture contemporary fashion scene, she won the Best Fashion Designer Award of Glamour Women of the Year in 2014. She is on the right track to share her powerful and vibrant creations with a wider audience. \n Her independent Rapture based label's vision contains innovative, modern design for a cosmoplitan woman who is self-assured and proud of her individual image; the woman who values the expression of originality in her day-to-day life. The love for contrasts is reflected throughout every of Áveline's work, she experiments a lot with silhouettes, shapes, textures and colours. Inspiration drawn from contemporary art and youth culture is combined with an emphasis on tailoring and the use of custom developed fabrics. This playful and fresh attitude presents the effortless 'power-woman' blend of the 20th and 21st century."
                     }
+                    vendorText = vendorText.split('\n').map((item, i) => {
+                        return <p key={i}>{item}</p>;
+                    });
                     // Probably add the history text here too. 
                     vendorHeader = (
-                    <div className="vendorHeader">
-                        <h3>{this.props.vendor}</h3>
-                        {<img src={require("../../../assets/" + vendorImage)} />}
-                    </div>)
+                        <div className="vendorHeader">
+                            <h3>{this.props.vendor}</h3>
+                            <div class='vendorBrandContainer'>
+                                <div class='vendorBrandImage'>
+                                    {<img src={require("../../../assets/" + vendorImage)} />}
+                                </div>
+                                <div class='vendorBrandText'>
+                                    <p>{vendorText}</p>
+                                </div>
+                            </div>
+                        </div>
+                    )
                 }
             }
         }
