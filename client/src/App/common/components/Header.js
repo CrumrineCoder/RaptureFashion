1ad87578d8b8e6f1816eedc986009d4dcc204eea
CrumrineCoder <crumrinecoding@gmail.com>
2019-05-13 19:54:08 -0400

    Header done for now

--- client/src/App/common/components/Header.js
+++ client/src/App/common/components/Header.js
@@ -12,7 +12,7 @@ import {
 	DropdownMenu
 } from 'reactstrap';
 import { connect } from 'react-redux';
-
+import { Link } from 'react-router-dom';
 //import PropTypes from 'prop-types';
 
 // Header hosts navigation info at the top of the screen. It appears on all pages.
@@ -52,10 +52,10 @@ class Header extends Component {
 					<Collapse isOpen={this.state.isOpen} navbar>
 						<Nav className="ml-auto" navbar>
 							<NavItem>
-								<NavLink href="#/">Home</NavLink>
+								<NavLink className="raptureFashionHeaderDropdownMenuLink" href="#/">Home</NavLink>
 							</NavItem>
 							<NavItem>
-								<NavLink href="#/about">About</NavLink>
+								<NavLink className="raptureFashionHeaderDropdownMenuLink" href="#/about">About</NavLink>
 							</NavItem>
 							<NavItem>
 								<Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggleDropdown}>
@@ -71,25 +71,27 @@ class Header extends Component {
 								</Dropdown>
 							</NavItem>
 							<NavItem>
-								<NavLink href="#/categories/all">All</NavLink>
+								<NavLink className="raptureFashionHeaderDropdownMenuLink" href="#/categories/all">All</NavLink>
 							</NavItem>
 							<NavItem>
-								<NavLink href="#/categories/dresses">Dresses</NavLink>
+								<NavLink className="raptureFashionHeaderDropdownMenuLink"href="#/categories/dresses">Dresses</NavLink>
 							</NavItem>
 							<NavItem>
-								<NavLink href="#/categories/shoes">Shoes</NavLink>
+								<NavLink className="raptureFashionHeaderDropdownMenuLink" href="#/categories/shoes">Shoes</NavLink>
 							</NavItem>
 							<NavItem>
-								<NavLink href="#/categories/accessories">Accessories</NavLink>
+								<NavLink className="raptureFashionHeaderDropdownMenuLink" href="#/categories/accessories">Accessories</NavLink>
 							</NavItem>
 							<NavItem>
-								<NavLink href="#/categories/hats">Hats</NavLink>
+								<NavLink className="raptureFashionHeaderDropdownMenuLink" href="#/categories/hats">Hats</NavLink>
 							</NavItem>
 							<NavItem className="cartRelativeItemWrapper">
-								<div className="cartWrapper">
-									<a className="cartLink" href="#/bag">
-										<span className="cartQuantity">{this.props.cartAmount}</span>
-									</a>
+								<div className="cartWrapper2">
+									<Link to="/bag">
+										<i className="fas fa-briefcase cartLink2 raptureFashionHeaderDropdownMenuLink">
+											<span className="cartQuantity2">{this.props.cartAmount}</span>
+										</i>
+									</Link>
 								</div>
 							</NavItem>
 						</Nav>
@@ -99,6 +101,9 @@ class Header extends Component {
 		)
 	}
 }
+/*	<a className="cartLink" href="#/bag">
+										<span className="cartQuantity">{this.props.cartAmount}</span>
+									</a>*/
 
 function mapStateToProps(state) {
 	const cartAmount = state.home.cart.checkout.lineItems.length;
